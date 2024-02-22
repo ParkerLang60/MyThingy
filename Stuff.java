fb0b75ee01363cc89e5c5ced0e02210d87f2afb6
cmonty21317 <CM67@students.ptcollege.edu>
2024-02-22 11:19:30 -0500

    Update Stuff.java

--- Stuff.java
+++ Stuff.java
@@ -1,5 +1,7 @@
 public class Stuff {
-
+    public static void andThings(){
+        System.out.println("Stuff and things I guess");
+    }
     public static void main(String[] args) {
         System.out.println("The world is bad.");
     }
