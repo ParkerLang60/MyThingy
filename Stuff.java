304eb33c1baa7dd7f53d69127f9c2dc6c55fc06c
streagle <63530532+streagle@users.noreply.github.com>
2024-02-22 11:20:02 -0500

    Update Stuff.java

--- Stuff.java
+++ Stuff.java
@@ -2,5 +2,6 @@ public class Stuff {
 
     public static void main(String[] args) {
         System.out.println("The world is bad.");
+        System.out.println("The world is not bad enough.");
     }
 }
